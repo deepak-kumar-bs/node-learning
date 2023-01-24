53e1734425d7026942f0ea6cadb5027fc28162ce
deepak-kumar-bs <deepak.kumar@bigsteptech.com>
2023-01-24 10:55:04 +0530

    optimised code

--- filesync.js
+++ filesync.js
@@ -1,61 +1,31 @@
 var fs = require('fs');
-// const config = require('./config.json');
-// console.log(config);
 
-// read json from file using async
-// fs.readFile('./config.json', null, (error,data) => {
-//     if(error){
-//         console.log(error);
-//         return;
-//     }
-
-//     console.log("async method");
-//     console.log(JSON.parse(data));
-// })
-
-// read json from file using sync
-// const data = fs.readFileSync('./config.json');
-// console.log("sync method");
-// console.log(JSON.parse(data));
-
-// write json in a file using async method
-// const x = {"color" : "red", "new":"yes"};
-// data = fs.writeFile('./config.json', JSON.stringify(x),(error) =>{
-//     if(error){
-//         console.log('An error has occurred');
-//         return;
-//     }
-//     console.log('Data written');
-// });
-
-// write json in a file using sync method
-// const x = {"color" : "red", "new":"yes"};
-// data = fs.writeFileSync('./config.json', JSON.stringify(x));
-// console.log('Data has written');
-
-// append json in a file using async method
-// fs.readFile('./config.json', null, (error, data) => {
-//     if(error){
-//         console.log('Failed to read data.');
-//     }
-//     jsonData = JSON.parse(data);
-//     jsonData[0].createdAt = Date();
-//     fs.writeFile('./config.json', JSON.stringify(jsonData), (err) => {
-//         if(err){
-//             console.log('Failed to write updated data to file.');
-//         }
-//         console.log('json data appended.');
-
-//     })
-// });
-
+// append json data in a file sync
 filedata = fs.readFileSync('./config.json');
 filedataJson = JSON.parse(filedata);
-filedataJson[0].jersyColor = "blue";
-filedataJson[0].new = "no";
-
+newJson = {"firstname":"Ishan","lastname":"Kishan","runs":3000,"iplTeam":"Mumbai","wickets":1,"matchPlayed":22};
+filedataJson.push(newJson);
 fs.writeFileSync('./config.json', JSON.stringify(filedataJson));
-console.log('json data updated.');
+console.log('One new player added');
+
+
+// append json data in a file async
+fs.readFile('./config.json', null, (error, data) => {
+    if(error){
+        console.log('Failed to read data.');
+    }
+    jsonData = JSON.parse(data);
+    jsonData.push({"firstname":"Surya Kumar","lastname":"Yadav","runs":1500,"iplTeam":"Gujrat","wickets":2,"matchPlayed":30});
+    fs.writeFile('./config.json', JSON.stringify(jsonData), (err) => {
+        if(err){
+            console.log('Failed to write data.');
+        }
+        console.log('One player added.');
+    })
+});
+
+
+
 
 
 
